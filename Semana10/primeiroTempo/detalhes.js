14f55d3f6a20c50695b71c23153c7dba6cbd28c4
ARTHUR PEIXOTO SCHILLER <202402851756@alunos.ibmec.edu.br>
2024-11-11 08:09:14 -0300

    Senha pelo local storage

---
+++ Semana10/primeiroTempo/detalhes.js
@@ -0,0 +1,61 @@
+const params = new URLSearchParams(window.location.search) // window.location é a URL da sua janela;
+
+const ids = params.get("id")
+
+const container = document.getElementById('container3');
+
+const pega_json = async (caminho) => {
+    const resposta = await fetch(caminho + ids);
+    console.log(resposta)
+    const dados = await resposta.json(); // transformando a string em json 
+    return dados
+}
+
+const acha_cookie = (chave) => {
+    const lista_de_cookies = document.cookie.split("; ")
+    const procurando = lista_de_cookies.find(
+        (e) => e.startsWith(`${chave}=`)
+    )
+    return procurando.split('=')[1]
+}
+
+const montarCard = (atleta) => {
+    const cartao = document.createElement("article");
+    const nome = document.createElement("h1");
+    const imagem = document.createElement("img");
+    const descricao = document.createElement("p");
+    const link = document.createElement("a")
+
+    nome.innerHTML = atleta.nome;
+    cartao.appendChild(nome);
+
+    imagem.src = atleta.imagem;
+    cartao.appendChild(imagem);
+    
+    descricao.innerHTML = atleta.detalhes;
+    cartao.appendChild(descricao);
+
+    link.innerHTML = "Saiba mais...";
+    link.href = `detalhes.html?id=${atleta.id}`
+    cartao.appendChild(link);
+
+    container.appendChild(cartao)
+}
+
+if (sessionStorage.getItem('logado')) {
+
+} else {
+    document.body.innerHTML = `<h1>Você precisa estar loagado</h1>`
+}
+pega_json(`https://botafogo-atletas.mange.li/2024-1/`).then(
+    (atleta) => montarCard(atleta)
+)
+
+
+
+console.log(acha_cookie("id"))
+
+console.log(localStorage.getItem('id'))
+
+const dadosss = JSON.parse(localStorage.getItem('atleta'))
+console.log(dadosss.nome)
---
+++ Semana10/primeiroTempo/index.js
@@ -0,0 +1,84 @@
+const pega_json = async (caminho) => {
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json(); // transformando a string em json
+    return dados
+}
+   
+const manipulaClick = (e) => {
+    const id = e.target.closest('article').dataset.id
+    const nome = e.currentTarget.dataset.nome
+    //cookie
+    document.cookie = `id=${id}`
+    document.cookie = `nome=${nome}`
+
+    sessionStorage.setItem('id', id)
+    sessionStorage.setItem('atleta', JSON.stringify(e.currentTarget.dataset))
+
+    localStorage.setItem('id', id)
+    localStorage.setItem('atleta', JSON.stringify(e.currentTarget.dataset))
+
+    window.location = `detalhes.html?id=${id}`
+}
+
+
+
+// let conteudo = ''
+const container = document.getElementById('container3');
+
+const montarCard = (atleta) => {
+    const cartao = document.createElement("article");
+    const nome = document.createElement("h1");
+    const imagem = document.createElement("img");
+    const descricao = document.createElement("p");
+    const link = document.createElement("a")
+    const span_id = document.createElement('span');
+
+    nome.innerHTML = atleta.nome;
+    cartao.appendChild(nome);
+
+    imagem.src = atleta.imagem;
+    cartao.appendChild(imagem);
+    
+    descricao.innerHTML = atleta.detalhes;
+    cartao.appendChild(descricao);
+
+    // link.innerHTML = "Saiba mais...";
+    // link.href = `detalhes.html?id=${atleta.id}`
+    // cartao.appendChild(link);
+
+    cartao.dataset.id = atleta.id;
+    cartao.dataset.nome = atleta.nome
+
+    span_id.innerHTML = atleta.id
+    cartao.appendChild(span_id)
+
+    cartao.onclick = manipulaClick
+
+
+
+    container.appendChild(cartao)
+}
+
+
+pega_json('https://botafogo-atletas.mange.li/2024-1/all').then(
+    ( retorno ) => {
+        retorno.forEach((atleta) => montarCard(atleta));
+        console.log("Isso imprime depois");
+    }
+        
+)
+
+
+const verificaSenha = () => {
+    const entrada = document.getElementById('senha').value;
+    const senha = 'casa';
+    if (senha === entrada) {
+        sessionStorage.setItem('logado', 'sim')
+        alert("Logado")
+    } else {
+        alert('Senha Incoreta')
+    }
+}
+
+
+document.getElementById('logout').onclick = () => {sessionStorage.removeItem('logado'); alert('saiu')}
